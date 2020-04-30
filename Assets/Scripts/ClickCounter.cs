59de4e53154e23d768f92ec789a6cdee69298b87
DanaWoltz <dw952717@ohio.edu>
2020-04-30 01:05:27 -0400

    Update!
    hi sorry i forgot to put the project inside the repo so it was all on the same level. please enjoy!

---
+++ Assets/Scripts/ClickCounter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickCounter : MonoBehaviour
+{
+    public int click = 0;
+    public Text score;
+    int index = 0;
+    //int newIndex = 0;
+    public List<Sprite> images;
+    public Image backgroundContainer;
+    
+    void Start()
+    {
+        score = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        score.text = "Clicks:" + click;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            click += 1;
+            if (click % 10 == 0)
+            {
+                CycleImages();
+            }
+        }
+    }
+
+    void CycleImages()
+    {
+        int newIndex = index + 1;
+        if(newIndex >= images.Count)
+        {
+            newIndex = 0;
+        }
+        
+        index = newIndex;
+        
+        backgroundContainer.sprite = images[index];
+
+    }
+}  
